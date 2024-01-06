d660fb5d165c7b5b979b66f7710e33f42fd19998
Kleps <axel.informatica.est22@gmail.com>
2024-01-06 14:49:53 -0600

    Script

--- faq-accordion/js/script.js
+++ faq-accordion/js/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const questionContainers = document.querySelectorAll(".faq-question-container");
+
+const questionTitleContainers = document.querySelectorAll(
+  ".faq-question-container-title-container"
+);
+
+const contentContainers = document.querySelectorAll(
+  ".faq-question-content-container"
+);
+
+questionTitleContainers.forEach((element, index) => {
+  element.addEventListener("click", function () {
+    contentContainers.forEach((element) => {
+      if (!(contentContainers[index] === element)) {
+        element.classList.add("hidden");
+        element.classList.remove("active");
+      } else {
+        element.classList.toggle("hidden");
+        element.classList.toggle("active");
+      }
+    });
+
+    questionContainers.forEach((element) => {
+      if (!(questionContainers[index] === element)) {
+        element.classList.remove("active");
+      } else {
+        element.classList.toggle("active");
+      }
+    });
+  });
+});
