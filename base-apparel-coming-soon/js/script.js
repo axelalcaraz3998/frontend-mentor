2a6e31c6d205241cdb4939b65eb98fa3ece933bf
klepzs <axel.informatica.est22@gmail.com>
2024-02-21 13:28:37 -0600

    Script

--- base-apparel-coming-soon/js/script.js
+++ base-apparel-coming-soon/js/script.js
@@ -1,1 +1,24 @@
-console.log("script.js loaded");
+const form = document.querySelector("#form-container");
+const emailInput = document.querySelector("#email-input");
+const errorMsg = document.querySelector("#error-msg");
+
+function validateEmail(email) {
+  const regexEmail =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return regexEmail.test(email);
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = emailInput.value;
+  const isValid = validateEmail(email);
+
+  if (!isValid) {
+    emailInput.classList.add("error");
+    errorMsg.classList.add("error");
+  } else {
+    emailInput.classList.remove("error");
+    errorMsg.classList.remove("error");
+  }
+});
