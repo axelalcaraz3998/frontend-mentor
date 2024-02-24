b7acda457e55c66014ee8dc9f39fcc18efd25994
klepzs <axel.informatica.est22@gmail.com>
2024-02-24 14:07:41 -0600

    Script

--- intro-component-with-signup-form/js/script.js
+++ intro-component-with-signup-form/js/script.js
@@ -1,1 +1,48 @@
-console.log("Script.js loaded");
+const formElement = document.querySelector("form");
+
+const fNameInput = document.getElementById("fName-input");
+const lNameInput = document.getElementById("lName-input");
+const emailInput = document.getElementById("email-input");
+const passwordInput = document.getElementById("password-input");
+
+const fNameError = document.getElementById("fName-error");
+const lNameError = document.getElementById("lName-error");
+const emailError = document.getElementById("email-error");
+const passwordError = document.getElementById("password-error");
+
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Validation
+  if (fNameInput.value === "") {
+    fNameInput.classList.add("invalid");
+    fNameError.classList.add("invalid");
+  } else {
+    fNameInput.classList.remove("invalid");
+    fNameError.classList.remove("invalid");
+  }
+
+  if (lNameInput.value === "") {
+    lNameInput.classList.add("invalid");
+    lNameError.classList.add("invalid");
+  } else {
+    lNameInput.classList.remove("invalid");
+    lNameError.classList.remove("invalid");
+  }
+
+  if (emailInput.value === "") {
+    emailInput.classList.add("invalid");
+    emailError.classList.add("invalid");
+  } else {
+    emailInput.classList.remove("invalid");
+    emailError.classList.remove("invalid");
+  }
+
+  if (passwordInput.value === "") {
+    passwordInput.classList.add("invalid");
+    passwordError.classList.add("invalid");
+  } else {
+    passwordInput.classList.remove("invalid");
+    passwordError.classList.remove("invalid");
+  }
+});
