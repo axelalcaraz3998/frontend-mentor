f22031952ad9c202b6d83afda38a1f2d9c0336f3
klepzs <axel.informatica.est22@gmail.com>
2024-07-23 17:30:32 -0600

    Desktop dropdown menu and tablet design

--- intro-section-with-dropdown-navigation/js/script.js
+++ intro-section-with-dropdown-navigation/js/script.js
@@ -1,1 +1,37 @@
-console.log("script.js loaded");
+const navbarList = document.querySelector(".header__left__navbar__list");
+const navLinks = document.querySelectorAll(".header__left__navbar__list__item");
+
+// Close dropdown when clicked outside
+window.addEventListener("click", (e) => {
+  const navLink = e.target.closest(".header__left__navbar__list__item");
+
+  if (!navLink) {
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+    });
+  }
+});
+
+// Toggle dropdown
+navbarList.addEventListener("click", (e) => {
+  // Get clicked nav link element
+  const navLink = e.target.closest(".header__left__navbar__list__item");
+
+  // If clicked element is not a nav link, return
+  if (!navLink) {
+    return;
+  }
+
+  // Toggle clicked dropdown
+  navLink.classList.toggle("active");
+
+  // Close all other active dropdowns
+  navLinks.forEach((link) => {
+    // If clicked nav link is the same as the current nav link, don't close it
+    if (link === navLink) {
+      return;
+    }
+
+    link.classList.remove("active");
+  });
+});
